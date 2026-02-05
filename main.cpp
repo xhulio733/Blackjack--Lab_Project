13eeef3052943058a2ed59dddbdfa8799e6fb836
Xhulio <xhulio.vasiu.stud@tulliobuzzi.edu.it>
2026-02-05 17:09:42 +0100

    Finestra grafica funzionante

--- main.cpp
+++ main.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
+#include <SFML/Graphics.hpp> // include tutto ciò che serve per graphics, window, system
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Blackjack");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
